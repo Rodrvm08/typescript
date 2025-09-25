2f6ebaffc6ffce6f7eed6dab110190540193b100
Rodrigo Vilela <Rodrvm08@gmail.com>
2025-09-25 14:18:42 -0300

    Add files via upload

---
+++ exercicio.ts
@@ -0,0 +1,10 @@
+function multiplicar(valA: number, ValB: number): number {
+    return valA * ValB;
+}
+
+function saudar(nome: string): string {
+    return `Olá ${nome}`;
+}
+
+console.log(multiplicar(5, 6));
+console.log(saudar("João"));
